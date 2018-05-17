e22ea02dc926cc83a10b7b0bb656bfb93eb38b70
Ze'ev Klapow <zklapow@hubspot.com>
2018-05-17 17:56:24 -0400

    map more pins

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,24 @@ pub struct Pins {
 
     pub tx: gpio::Pa10<Input<Floating>>,
     pub rx: gpio::Pa11<Input<Floating>>,
+
+    pub aref: gpio::Pa3<Input<Floating>>,
     
     pub d2: gpio::Pa14<Input<Floating>>,
     pub d3: gpio::Pa9<Input<Floating>>,
     pub d4: gpio::Pa8<Input<Floating>>,
+    pub d5: gpio::Pa15<Input<Floating>>,
+    pub d6: gpio::Pa20<Input<Floating>>,
+    pub d7: gpio::Pa21<Input<Floating>>,
+    pub d9: gpio::Pa7<Input<Floating>>,
+    pub d10: gpio::Pa18<Input<Floating>>,
+    pub d11: gpio::Pa16<Input<Floating>>,
+    pub d12: gpio::Pa19<Input<Floating>>,
+
+    pub a3: gpio::Pa4<Input<Floating>>,
+    pub a2: gpio::Pb9<Input<Floating>>,
+    pub a1: gpio::Pb8<Input<Floating>>,
+    pub a0: gpio::Pa2<Input<Floating>>,
 
     /// Digital pin number 13, which is also attached to
     /// the red LED.  PWM capable.
@@ -36,9 +50,21 @@ pub fn pins(port: atsamd21g18a::PORT) -> Pins {
         tx: pins.pa10,
         rx: pins.pa11,
 
+        aref: pins.pa3,
         d2: pins.pa14,
         d3: pins.pa9,
         d4: pins.pa8,
+        d5: pins.pa15,
+        d6: pins.pa20,
+        d7: pins.pa21,
+        d9: pins.pa7,
+        d10: pins.pa18,
+        d11: pins.pa16,
+        d12: pins.pa19,
+        a3: pins.pa4,
+        a2: pins.pb9,
+        a1: pins.pb8,
+        a0: pins.pa2,
 
         led: pins.pa17,
         tx_led: pins.pa27,
